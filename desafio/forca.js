438b50d154682cb4eb64662872347012330976c1
Gaspor <gaspor991@gmail.com>
2022-07-20 20:45:15 -0300

    refactoring: change var to let

--- desafio/forca.js
+++ desafio/forca.js
@@ -8,19 +8,23 @@ class Forca {
     }
 
     chutar(letra) {
+        /* Verifica se o que foi digitado é uma letra */
         if(!letra.match(/[A-Za-z]/g)) {
             console.log("Digite apenas letras!");
             return false;
+
         }
         
         const letter = letra.toLowerCase();
 
+        /* Verifica se o usuário digitou apenas uma letra */
         if (letter.length > 1) {
             console.log("Digite apenas uma letra por vez!");
             return false;
 
         }
 
+        /* Verifica se a letra já foi digitada */
         if (this.attempt.includes(letter)) {
             console.log("Essa letra já foi usada!");
             return false;
@@ -29,6 +33,7 @@ class Forca {
 
         this.attempt += letter;
         
+        /* Verifica se a letra existe na palavra */
         if (!this.word.includes(letter)) {
           this.lifes--;
           return false;
@@ -61,7 +66,7 @@ class Forca {
     }
 
     replaceAnswer(letter) {
-        for (var i = 0; i < this.word.length; i++) {
+        for (let i = 0; i < this.word.length; i++) {
             if (this.word[i] == letter) {
                 this.answer[i] = letter;
 
